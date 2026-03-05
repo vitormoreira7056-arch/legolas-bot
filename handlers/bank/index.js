a2d7f28dcc5805b8df8cd0cf190ff5e3b6421b43
Vitor Tracer <vitormoreira7056@gmail.com>
2026-03-05 15:56:44 -0300

    Update index.js

--- handlers/bank/index.js
+++ handlers/bank/index.js
@@ -1,33 +1,32 @@
-const BankEmbeds = require('./bankEmbeds');
 const BankCore = require('./bankCore');
-const BankVendas = require('./bankVendas');
+const bankVendas = require('./bankVendas');
+const bankEmbeds = require('./bankEmbeds');
 
-// Compatibilidade com código antigo - exporta tudo junto
-class BankHandler {
+// 🆕 CORREÇÃO: Adicionados métodos de venda que estavam faltando
+module.exports = {
+  // Métodos do BankCore
+  getUser: BankCore.getUser,
+  updateUser: BankCore.updateUser,
+  addTransaction: BankCore.addTransaction,
+  requestWithdrawal: BankCore.requestWithdrawal,
+  approveWithdrawal: BankCore.approveWithdrawal,
+  rejectWithdrawal: BankCore.rejectWithdrawal,
+  requestLoan: BankCore.requestLoan,
+  approveLoan: BankCore.approveLoan,
+  rejectLoan: BankCore.rejectLoan,
+  transfer: BankCore.transfer,
+  getBalance: BankCore.getBalance,
+  deposit: BankCore.deposit,
+  addGuildBalance: BankCore.addGuildBalance,
+  getGuildBalance: BankCore.getGuildBalance,
+  
+  // 🆕 ADICIONADO: Métodos de venda do bankVendas
+  approveVenda: bankVendas.approveVenda,
+  rejectVenda: bankVendas.rejectVenda,
+  processVenda: bankVendas.processVenda,
+  
   // Embeds
-  static createWithdrawalRequestEmbed(...args) { return BankEmbeds.createWithdrawalRequestEmbed(...args); }
-  static createWithdrawalButtons(...args) { return BankEmbeds.createWithdrawalButtons(...args); }
-  static createLoanRequestEmbed(...args) { return BankEmbeds.createLoanRequestEmbed(...args); }
-  static createLoanButtons(...args) { return BankEmbeds.createLoanButtons(...args); }
-  static createBalanceEmbed(...args) { return BankEmbeds.createBalanceEmbed(...args); }
-  static createVendaEmbed(...args) { return BankEmbeds.createVendaEmbed(...args); }
-  static createVendaButtons(...args) { return BankEmbeds.createVendaButtons(...args); }
-
-  // Core
-  static requestWithdrawal(...args) { return BankCore.requestWithdrawal(...args); }
-  static requestLoan(...args) { return BankCore.requestLoan(...args); }
-  static approveWithdrawal(...args) { return BankCore.approveWithdrawal(...args); }
-  static rejectWithdrawal(...args) { return BankCore.rejectWithdrawal(...args); }
-  static approveLoan(...args) { return BankCore.approveLoan(...args); }
-  static rejectLoan(...args) { return BankCore.rejectLoan(...args); }
-  static depositManual(...args) { return BankCore.depositManual(...args); }
-
-  // Vendas
-  static approveVenda(...args) { return BankVendas.approveVenda(...args); }
-  static rejectVenda(...args) { return BankVendas.rejectVenda(...args); }
-
-  // Cores
-  static get colors() { return BankEmbeds.colors; }
-}
-
-module.exports = BankHandler;
+  createWithdrawalEmbed: bankEmbeds.createWithdrawalEmbed,
+  createLoanEmbed: bankEmbeds.createLoanEmbed,
+  createBalanceEmbed: bankEmbeds.createBalanceEmbed
+};
